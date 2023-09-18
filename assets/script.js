2853d173afefb59ffe4a9136e048a6684c50ad72
Sharkman478 <steviehelber@gmail.com>
2023-09-18 10:48:03 -0400

    final changes and style

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-
-// when id incorrect is input in question timer reduces by 15 seconds and button goes to next question else nothing happens to timer and button goes to next question
-// if (incorrect) {
-//     timer--15;
-// } else {
-//     timer=timer;
-// }
-
-
